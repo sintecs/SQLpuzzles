489e0a09eab2372fecfc030f6c4200dd58a8c9bf
sintecs <sintecs@users.noreply.github.com>
2020-09-13 15:30:56 -0700

    Added new column SizeID values

--- SQLServer/static_data/Puzzle.sql
+++ SQLServer/static_data/Puzzle.sql
@@ -5,14 +5,15 @@ INSERT INTO dbo.Puzzle
   PuzzleID
  ,PuzzleTypeID
  ,Solved
+ ,SizeID
 )
 VALUES 
-    (1, 1, 0)
-   ,(2, 1, 0)
-   ,(3, 1, 0)
-   ,(4, 1, 0)
-   ,(5, 1, 0)
-   ,(6, 2, 0)
+    (1, 1, 0, 1)
+   ,(2, 1, 0, 1)
+   ,(3, 1, 0, 1)
+   ,(4, 1, 0, 1)
+   ,(5, 1, 0, 1)
+   ,(6, 2, 0, 1)
 
 
 SET IDENTITY_INSERT dbo.Puzzle OFF
