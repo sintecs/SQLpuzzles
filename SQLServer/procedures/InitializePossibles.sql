0d71c99b4ce5e025b3badf687041713ca840c623
sintecs <sintecs@users.noreply.github.com>
2020-09-13 16:15:34 -0700

    Initial Check-in

---
+++ SQLServer/procedures/InitializePossibles.sql
@@ -0,0 +1,26 @@
+--------------------------------------------------------------------------------
+--                                                                            --
+-- This function will fill the solving table with the possible values that    --
+-- can be placed into the open/unsolved squares.                              --
+--                                                                            --
+--------------------------------------------------------------------------------
+CREATE OR ALTER PROCEDURE InitializePossibles
+(
+   @PuzzleID                      INT
+   ,@PuzzleSize                   INT
+) AS
+BEGIN
+
+   -- Initialize table by removing any old data for this puzzle
+   DELETE
+      dbo.SquareDataPossible 
+   WHERE 
+      PuzzleID = @PuzzleID;
+   
+   EXEC dbo.FillSquareDataPossible
+      @PuzzleID
+      ,@PuzzleSize;
+
+END
+
+GO
